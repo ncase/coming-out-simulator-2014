9c5a77227ac9c634fb54c12b7b5bd24a7c750cf3
Nicky Case <n@ncase.me>
2014-06-30 03:50:32 -0700

    fix

--- js/game/outro.js
+++ js/game/outro.js
@@ -550,7 +550,7 @@ function Finale_4(message){
 	queue(function(){
 		document.getElementById("game").setAttribute("screen","blank");
 	},1000);
-	queue(ClearScene,0);
+	//queue(ClearScene,0); // coz the sound's cool!
 	queue(function(){
 		document.getElementById("game").setAttribute("screen","credits");
 	},0);
