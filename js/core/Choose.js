bba0c980e6fc70cc8aa6ac373413ffde93f52c9c
Kraig Walker <kraig_walker@me.com>
2014-07-10 07:56:42 +0100

    Did some code cleanup, added .editorconfig, cleaned up index.html

--- js/core/Choose.js
+++ js/core/Choose.js
@@ -1,28 +1,28 @@
+/*global window */
 var $ = {};
 
 ////////////
 
 window.requestAnimationFrame = window.requestAnimationFrame
-	|| window.mozRequestAnimationFrame
-	|| window.webkitRequestAnimationFrame
-	|| window.msRequestAnimationFrame;
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame;
 
 var lastTimestep = null;
 var ticker = 0;
 function step(timestamp) {
 
-	// Tick!
-	publish("tick",[]);
-	
-	// Do the next thing in the queue
-	if(_queue.length>0){
-		ticker += 1000/60; // HARD CODED. So it pauses when you're away.
-		while(_queue.length>0 && ticker>_queue[0].time){ // For instantaneous events, like Show.
-			var item = _queue.shift();
-			item.callback();
-		}
-	}
+    // Tick!
+    publish("tick", []);
 
+    // Do the next thing in the queue
+    if(_queue.length > 0) {
+	   ticker += 1000/60; // HARD CODED. So it pauses when you're away.
+        while (_queue.length > 0 && ticker > _queue[0].time) { // For instantaneous events, like Show.
+            var item = _queue.shift();
+            item.callback();
+        }
+    }
 	requestAnimationFrame(step);
 }
 requestAnimationFrame(step);
@@ -32,29 +32,29 @@ requestAnimationFrame(step);
 var timer = 0;
 var ticker = 0;
 
-function skipStep(){
-	if(_queue.length>0){
+function skipStep() {
+	if(_queue.length > 0) {
 		var item = _queue.shift();
 		item.callback();
 		ticker = item.time;
 	}
 }
 
-function resetTimer(){
+function resetTimer() {
 	timer = 0;
 	ticker = 0;
 }
 
 var TIMER_GAP = 100;
-function getDuration(message){
+function getDuration(message) {
 	// Approx 6 words per second, or 160ms per word. Plus 800ms just in case.
-	return 800 + message.split(" ").length*160;
+	return 800 + message.split(" ").length * 160;
 }
 
 var _queue = [];
 function queue(callback, duration, instantaneous){
 	duration = duration || 0;
-	if(!instantaneous){
+	if(!instantaneous) {
 		timer += TIMER_GAP;
 	}
 	_queue.push({
@@ -64,39 +64,43 @@ function queue(callback, duration, instantaneous){
 	timer += duration;
 }
 
-function Character(character){
-	return function(message){
+/**
+ * Creates a new Character that can speak dialogue in the game
+ * @param {Object} options [description]
+ */
+function Character(options) {
+	return function(message) {
 		if(!message) return;
-		queue(function(){
+		queue(function() {
 			publish("say", [character, message]);
 		},getDuration(message));
 	};
 }
 
 function Choose(choices){
-	queue(function(){
+	queue(function() {
 		publish("choose", [choices]);
 		resetTimer();
 	});
 };
 
-function Show(){
+function Show() {
 	var args = arguments;
-	queue(function(){
+	queue(function() {
 		publish("show", args);
 	},0,true);
 }
-function PlaySound(){
+function PlaySound() {
 	var args = arguments;
-	queue(function(){
+	queue(function() {
 		publish("play", args);
-	},0,true);
+	}, 0, true);
 }
-function StopSound(){
+function StopSound() {
 	var args = arguments;
-	queue(function(){
+	queue(function() {
 		publish("stop", args);
-	},0,true);
+	}, 0, true);
 }
 
 function Wait(duration){
@@ -144,16 +148,16 @@ function Sprite(label,options){
 
 createjs.Sound.alternateExtensions = ["mp3"];
 function Sound(label,src){
-	
+
 	var deferred = Q.defer();
-	
+
 	createjs.Sound.registerSound({
 		id: label,
 		src: src
 	});
 
 	(function(label){
-		createjs.Sound.addEventListener("fileload", function(event){
+		createjs.Sound.addEventListener("fileload", function(event) {
 			if(event.id==label){
 				_resourcesLoaded++;
 				publish("resourceLoaded",[]);
