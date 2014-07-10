b59c67ee488d8cb0e875d413aff5456927ee9736
Kraig Walker <kraig_walker@me.com>
2014-07-10 07:56:42 +0100

    Did some code cleanup, added .editorconfig, cleaned up index.html

--- js/config/characters.js
+++ js/config/characters.js
@@ -1,17 +1,18 @@
+/*global Character*/
 // Nicky in the main menu & outro
-N = new Character({ align:"left", background:"#FFF", color:"#000", sound:"text_mid" });
+var N = new Character({ align: "left", background: "#FFF", color: "#000", sound: "text_mid" });
 
 // The player in the main menu & outro
-p = new Character({ align:"right", background:"#333", color:"#bbb", sound:"text_low" });
+var p = new Character({ align : "right", background : "#333", color: "#bbb", sound: "text_low" });
 
 // Nicky in the past, the one you can play
-n = new Character({ align:"right", background:"#79B8FE", sound:"text_mid" });
+var n = new Character({ align: "right", background: "#79B8FE", sound: "text_mid" });
 
 // Mother character
-m = new Character({ align:"left", background:"#FFF", sound:"text_high" });
+var m = new Character({ align: "left", background: "#FFF", sound: "text_high" });
 
 // Father character
-f = new Character({ align:"left", background:"#333", color:"#bbb", sound:"text_low" });
+var f = new Character({ align: "left", background: "#333", color: "#bbb", sound: "text_low" });
 
 // Jack, the boyfriend character
-j = new Character({ align:"left", background:"#FFF", sound:"text_high" });
+var j = new Character({ align: "left", background: "#FFF", sound: "text_high" });
